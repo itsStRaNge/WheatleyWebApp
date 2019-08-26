39d002a5fd658efe77dba5fc7385305a92346b13
gitlab+deploy-token-51397 <lukas.bernhard@park-here.eu>
2019-08-26 12:52:00 +0200

    try different group codes

--- rf_sender.py
+++ rf_sender.py
@@ -1,19 +1,21 @@
-from rpi_rf import RFDevice
-
+import time
 
 
 GROUP_NUMBER = "FFF00"
-
 GPIO_PIN = 0
 
+try:
+    from rpi_rf import RFDevice
 
-rfdevice = RFDevice(GPIO_PIN,
-                    tx_proto=1,
-                    tx_pulselength=300,
-                    tx_repeat=10,
-                    tx_length=12,
-                    rx_tolerance=60)
-rfdevice.enable_tx()
+    rfdevice = RFDevice(GPIO_PIN,
+                        tx_proto=1,
+                        tx_pulselength=300,
+                        tx_repeat=10,
+                        tx_length=12,
+                        rx_tolerance=60)
+    rfdevice.enable_tx()
+except RuntimeError:
+    pass
 
 
 def turn_socket_on(socket_nr):
@@ -26,10 +28,12 @@ def turn_socket_off(socket_nr):
 
 def _set_socket(socket_nr, state):
    code = _getCodeWordB(socket_nr, state)
-   print("length %s" % len(code))
+   # print("length %s" % len(code))
    print(code)
    rfdevice.tx_bin(code)
-   # rfdevice.tx_code(code=code)
+   time.sleep(0.2)
+   rfdevice.tx_code(code=code)
+   time.sleep(0.2)
 
 
 def _getCodeWordB(nChannelCode, state):
@@ -49,3 +53,12 @@ def _getCodeWordB(nChannelCode, state):
    sReturn += "\0"
 
    return sReturn  # int(sReturn, 16)
+
+if __name__ == '__main__':
+    id = 4
+    import itertools
+
+    for comb in itertools.product(["0", "F"], repeat=5):
+        GROUP_NUMBER = ''.join(comb)
+        turn_socket_on(id)
+
